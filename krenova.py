6369dc0c6dcb06e8de1daadb973c7a01e7858347
setyoriniokvi <setyoriniokviana@gmail.com>
2025-12-27 21:50:33 +0700

    ini benerin lagi ya
    lagi lagi benerin nih

--- krenova.py
+++ krenova.py
@@ -356,7 +356,7 @@ def stunting_risk_percent(hfa, wfa):
 
 
 ## ========= STREAMLIT
-st.set_page_config(page_title="Si Tumbuh")
+st.set_page_config(page_title="SI Tumbuh")
 
 # Custom CSS untuk mempercantik
 st.markdown("""
