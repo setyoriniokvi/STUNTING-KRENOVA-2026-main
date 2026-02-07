c9bae3de17c4986e7a2e8eed6b6e171d936cf288
Mayang Gumelar <gumelarmayang@gmail.com>
2026-02-07 10:56:20 +0700

    Update krenova.py
    Update versi 3 gemini api

--- krenova.py
+++ krenova.py
@@ -62,7 +62,7 @@ def get_ai_analysis(data_anak, status_z):
 """
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3-flash-preview",
             contents=prompt
         )
         return response.text
